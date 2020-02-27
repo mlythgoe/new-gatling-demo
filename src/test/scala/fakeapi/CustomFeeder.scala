46463712908d7d9766692572625c984e4f28bb4d
Mike Lythgoe <mike.lythgoe@bjss.com>
2020-02-27 14:00:17 +0000

    Tided up CustomFeeder

--- src/test/scala/fakeapi/CustomFeeder.scala
+++ src/test/scala/fakeapi/CustomFeeder.scala
@@ -1,29 +1,12 @@
 package scala.fakeapi
 
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
-import scala.util.Random
-
 class CustomFeeder extends Simulation {
 
   val httpConf = http.baseUrl("https://fakerestapi.azurewebsites.net/api/")
     .header("Accept", "application/json")
     .proxy(Proxy("localhost", 8888))
-
-  var idNumbers = (1 to 2).iterator
-
-//  val customFeeder = Iterator.continually(Map(
-//    "ID" -> idNumbers.next(),
-//    "Title" -> ("Game-" + getRandomString(5)),
-//    "Description" -> getRandomDate(now, rnd),
-//    "PageCount" -> rnd.nextInt(100),
-//    "Excerpt" -> ("Category-" + getRandomString(6)),
-//    "PublishDate" -> ("Rating-" + getRandomString(4))
-//  ))
 
   val customFeeder = csv("data/BookCsvFile.csv").circular
 
